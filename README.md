914572e2ceaa1f9a7826e2fcea6eee27ba1c4bda
Tonye Jack <jtonye@ymail.com>
2022-12-12 19:23:43 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,76 @@
+# json2file
+
+Generate file output from a JSON string.
+
+## Usage
+
+```yaml
+    - name: Set output
+      id: set-output
+      run: |
+        echo "color=green" >> $GITHUB_OUTPUT
+
+    - name: Generate file output from json
+      uses: tj-actions/json2file@v1
+      with:
+        directory: 'output'
+        outputs:  ${{ steps.set-output.outputs }}
+        keys: 'color'
+        extension: 'txt'
+```
+
+The above example will create a file named `color.txt` in the `output` directory with the contents `green`.
+
+
+## Inputs
+
+
+## CLI Reference
+Run `json2file --help` for more information.
+
+```bash
+$ json2file --help
+json2file
+Generate files from a JSON output.
+
+Usage:
+        json2file --keys=[KEYS] --outputs=[OUTPUT] --directory=[DIRECTORY] --extension=[EXTENSION]
+
+Options:
+
+-h, --help              Show this help message and exit.
+-v, --version           Show the version and exit.
+-k, --keys              The keys to use to generate the files. (Required)
+-o, --outputs           The JSON output to use. (Required)
+-d, --directory         The directory to output the files to. (Required)
+-e, --extension         The extension to use for the files. (Optional, defaults to txt)
+
+Example:
+        json2file --keys=foo,bar --outputs="{\"foo\": \"value1\", \"bar\": \"value2\"}" --directory=/tmp --extension=tx
+```
+
+
+*   Free software: [MIT license](LICENSE)
+
+If you feel generous and want to show some extra appreciation:
+
+[![Buy me a coffee][buymeacoffee-shield]][buymeacoffee]
+
+[buymeacoffee]: https://www.buymeacoffee.com/jackton1
+
+[buymeacoffee-shield]: https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png
+
+## Credits
+
+This package was created with [Cookiecutter](https://github.com/cookiecutter/cookiecutter) using [cookiecutter-action](https://github.com/tj-actions/cookiecutter-action)
+
+## Report Bugs
+
+Report bugs at https://github.com/tj-actions/json2file/issues.
+
+If you are reporting a bug, please include:
+
+*   Your operating system name and version.
+*   Any details about your workflow that might be helpful in troubleshooting.
+*   Detailed steps to reproduce the bug.
+
---
+++ src/main.rs
@@ -0,0 +1,312 @@
+use std::env;
+use std::fs::File;
+use std::io::{ErrorKind, Write};
+use std::path::PathBuf;
+
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+fn write_outputs(
+    keys: &Vec<String>,
+    output: &str,
+    output_directory: &PathBuf,
+    output_extension: &str,
+) {
+    let json: serde_json::Value = match serde_json::from_str(output) {
+        Ok(json) => json,
+        Err(e) => {
+            eprintln!("Error parsing output: {}", e);
+            std::process::exit(1);
+        }
+    };
+
+    // Create the output directory if it doesn't exist
+    if !output_directory.exists() {
+        match std::fs::create_dir_all(output_directory) {
+            Ok(_) => (),
+            Err(e) => {
+                eprintln!("Error creating output directory: {}", e);
+                std::process::exit(1);
+            }
+        }
+    }
+
+    for key in keys {
+        let file_name = &format!("{}.{}", key, output_extension);
+        let mut file = match File::create(output_directory.join(file_name)) {
+            Ok(file) => file,
+            Err(err) => {
+                if err.kind() == ErrorKind::AlreadyExists {
+                    eprintln!("File already exists: {}", file_name);
+                    std::process::exit(1);
+                } else {
+                    eprintln!("Failed to create file {}: {}", file_name, err);
+                    std::process::exit(1);
+                }
+            }
+        };
+
+        let value = match json.get(key) {
+            Some(value) => value,
+            None => {
+                eprintln!("Invalid key \"{}\" not found in output {}", key, output);
+                std::process::exit(1);
+            }
+        };
+
+        if let Err(err) = file.write_all(value.to_string().as_bytes()) {
+            eprintln!("Failed to write to file {}: {}", file_name, err);
+            std::process::exit(1);
+        }
+    }
+}
+
+fn get_args_as_vec(pattern: &str) -> Vec<String> {
+    let re = regex::Regex::new(pattern).unwrap();
+
+    return env::args()
+        .skip(1)
+        .filter(|arg| re.is_match(arg))
+        .map(|arg| {
+            arg.split("=")
+                .nth(1)
+                .unwrap_or_else(|| {
+                    eprintln!("Invalid option: {}", arg);
+                    std::process::exit(1);
+                })
+                .to_string()
+        })
+        .collect();
+}
+
+fn get_args_as_bool(pattern: &str) -> bool {
+    let re = regex::Regex::new(pattern).unwrap();
+
+    return env::args().skip(1).filter(|arg| re.is_match(arg)).count() > 0;
+}
+
+fn parse_keys() -> Result<Vec<String>, String> {
+    let keys = get_args_as_vec(r"^(--keys|-k)=");
+    // Split the keys by commas or spaces or newlines
+    let re = regex::Regex::new(r"[,\s\\n]+").unwrap();
+    let mut output: Vec<String> = Vec::new();
+
+    if keys.is_empty() {
+        Err("No keys provided, Please specify at least one key using --key=[KEY_NAME] or -k=[KEY_NAME].".to_string())
+    } else {
+        for key in keys {
+            if re.is_match(&key) {
+                output.extend(re.split(&key).map(|s| s.trim().to_string()));
+            } else {
+                output.push(key.trim().to_string());
+            }
+        }
+        Ok(output)
+    }
+}
+
+fn parse_outputs() -> Result<String, String> {
+    let outputs: Vec<String> = get_args_as_vec(r"^(--outputs|-o)=");
+
+    let outputs_len = outputs.len();
+
+    if outputs_len > 1 {
+        Err(format!(
+            "Too many outputs provided, expected 1, got {}.",
+            outputs_len
+        ))
+    } else if outputs.is_empty() {
+        Err(
+            "No outputs provided, Please specify an output using --outputs=[OUTPUT] or -o=[OUTPUT]."
+                .to_string()
+        )
+    } else {
+        Ok(outputs[0].clone())
+    }
+}
+
+fn parse_directory() -> Result<String, String> {
+    let directories: Vec<String> = get_args_as_vec(r"^(--directory|-d)=");
+
+    let directory_len = directories.len();
+
+    if directory_len > 1 {
+        Err(format!(
+            "Too many directories provided, expected 1, got {}.",
+            directory_len
+        ))
+    } else if directories.is_empty() {
+        Err(
+            "No directories provided, Please specify a directory using --directory=[DIRECTORY] or -d=[DIRECTORY]."
+                .to_string(),
+        )
+    } else {
+        Ok(directories[0].clone())
+    }
+}
+
+fn parse_output_extension() -> Result<String, String> {
+    let extensions: Vec<String> = get_args_as_vec(r"^(--extension|-e)=");
+
+    let extension_len = extensions.len();
+
+    if extension_len > 1 {
+        Err(format!(
+            "Too many extensions provided, expected 1, got {}.",
+            extension_len
+        ))
+    } else if extensions.is_empty() {
+        Ok("txt".to_string())
+    } else {
+        Ok(extensions[0].clone())
+    }
+}
+
+fn parse_help() -> bool {
+    return get_args_as_bool(r"^(--help|-h)$");
+}
+
+fn parse_version() -> bool {
+    return get_args_as_bool(r"^(--version|-v)$");
+}
+
+fn options_valid() -> bool {
+    // Check if the options are valid i.e if the user had a typo --h instead of --help
+    let valid_options = vec![
+        "--keys",
+        "-k",
+        "--outputs",
+        "-o",
+        "--directory",
+        "-d",
+        "--extension",
+        "-e",
+        "--help",
+        "-h",
+        "--version",
+        "-v",
+    ];
+
+    let invalid_options: Vec<String> = env::args()
+        .skip(1)
+        .filter(|arg| {
+            for option in &valid_options {
+                if arg.starts_with(option) {
+                    return false;
+                }
+            }
+            true
+        })
+        .collect();
+
+    if invalid_options.len() > 0 {
+        eprintln!("Invalid option(s): {:?}", invalid_options);
+        eprintln!("Please use --help or -h to see the available options.");
+        return false;
+    }
+
+    true
+}
+
+fn main() {
+    if !options_valid() {
+        std::process::exit(1);
+    }
+
+    let version = parse_version();
+
+    if version {
+        println!("json2file {}", VERSION);
+        std::process::exit(0);
+    }
+
+    let help = parse_help();
+
+    if help {
+        let example_outputs = r#"{\"foo\": \"value1\", \"bar\": \"value2\"}"#;
+        println!("json2file");
+        println!("Generate files from a JSON output.\n");
+        println!("Usage:\n\tjson2file --keys=[KEYS] --outputs=[OUTPUT] --directory=[DIRECTORY] --extension=[EXTENSION]\n");
+        println!("Options:\n");
+        println!("-h, --help\t\tShow this help message and exit.");
+        println!("-v, --version\t\tShow the version and exit.");
+        println!("-k, --keys\t\tThe keys to use to generate the files. (Required)");
+        println!("-o, --outputs\t\tThe JSON output to use. (Required)");
+        println!("-d, --directory\t\tThe directory to output the files to. (Required)");
+        println!(
+            "-e, --extension\t\tThe extension to use for the files. (Optional, defaults to txt)"
+        );
+        println!("\nExample:\n\tjson2file --keys=foo,bar --outputs=\"{}\" --directory=/tmp --extension=txt", example_outputs);
+        std::process::exit(0);
+    }
+
+    let keys = parse_keys().unwrap_or_else(|err| {
+        eprintln!("{}", err);
+        std::process::exit(1);
+    });
+
+    let outputs = parse_outputs().unwrap_or_else(|err| {
+        eprintln!("{}", err);
+        std::process::exit(1);
+    });
+
+    let current_directory = env::current_dir().unwrap_or_else(|err| {
+        eprintln!("Failed to get current directory: {}", err);
+        std::process::exit(1);
+    });
+
+    let directory = parse_directory().unwrap_or_else(|err| {
+        eprintln!("{}", err);
+        std::process::exit(1);
+    });
+
+    let output_extension = parse_output_extension().unwrap_or_else(|err| {
+        eprintln!("{}", err);
+        std::process::exit(1);
+    });
+
+    let output_directory = current_directory.join(directory);
+
+    write_outputs(&keys, &outputs, &output_directory, &output_extension);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::Read;
+
+    #[test]
+    fn test_write_outputs() {
+        // Create some test input for the write_outputs() function.
+        let keys = vec!["key1".to_string(), "key2".to_string()];
+        let output = r#"{
+            "key1": "value1",
+            "key2": "value2",
+            "key3": "value3"
+        }"#;
+
+        let output_directory = PathBuf::from("test");
+        let output_extension = "txt";
+
+        write_outputs(&keys, &output, &output_directory, &output_extension);
+
+        // Check that the files were created.
+        assert!(output_directory.join("key1.txt").exists());
+        assert!(output_directory.join("key2.txt").exists());
+
+        // Check that the files contain the correct values.
+        let mut file = File::open(output_directory.join("key1.txt")).unwrap();
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).unwrap();
+        assert_eq!(contents, "value1");
+
+        let mut file = File::open(output_directory.join("key2.txt")).unwrap();
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).unwrap();
+        assert_eq!(contents, "value2");
+
+        // Clean up the files and the test directory.
+        std::fs::remove_file(output_directory.join("key1.txt")).unwrap();
+        std::fs::remove_file(output_directory.join("key2.txt")).unwrap();
+        std::fs::remove_dir(output_directory).unwrap();
+    }
+}

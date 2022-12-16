7d70bc412225165b93262373244c420cf3f086ba
Tonye Jack <jtonye@ymail.com>
2022-12-15 20:43:22 -0700

    Updated entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 set -exuo pipefail
 
 INPUT_OUTPUTS="$(echo "$INPUT_OUTPUTS" | jq -r @json)"
+INPUT_KEYS="$(echo "$INPUT_KEYS" | jq -r @bash)"
 
 if [[ -z "$INPUT_BIN_PATH" ]]; then
   ## TODO: use "curl -sf https://[github releases - latest]] | PREFIX=. sh"
@@ -10,13 +11,7 @@ fi
 
 echo "Generating output using $INPUT_BIN_PATH..."
 
-read -r -d '' KEYS_STRING <<< "$INPUT_KEYS"
-
-mapfile -t KEYS_ARRAY <<< "$KEYS_STRING"
-
-echo "Keys: ${KEYS_ARRAY[*]}"
-
-$INPUT_BIN_PATH --keys="${KEYS_ARRAY[*]}" --outputs="$INPUT_OUTPUTS" \
+$INPUT_BIN_PATH --keys="${INPUT_KEYS}" --outputs="$INPUT_OUTPUTS" \
   --directory="$INPUT_DIRECTORY" --extension="$INPUT_EXTENSION" && exit_status=$? || exit_status=$?
 
 rm -f "$INPUT_BIN_PATH"

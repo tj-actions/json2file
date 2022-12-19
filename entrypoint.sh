72b11da607fa06dd8e301ab58876f3126abd2551
Tonye Jack <jtonye@ymail.com>
2022-12-19 02:49:47 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,11 +21,11 @@ if [[ -z "$INPUT_BIN_PATH" ]]; then
   fi
 
   if [[ "$ARCHIVE" == "zip" ]]; then
-    curl -sL https://github.com/tj-actions/json2file/releases/download/"$LATEST_VERSION"/json2file_"$LATEST_VERSION"_"$TARGET"."$ARCHIVE" -o "$TEMP_DIR"/json2file.zip
+    curl -sf https://github.com/tj-actions/json2file/releases/download/"$LATEST_VERSION"/json2file_"$LATEST_VERSION"_"$TARGET"."$ARCHIVE" -o "$TEMP_DIR"/json2file.zip
     unzip -q "$TEMP_DIR"/json2file.zip -d "$TEMP_DIR"
     chmod +x "$TEMP_DIR"/json2file
   else
-    curl -sL https://github.com/tj-actions/json2file/releases/download/"$LATEST_VERSION"/json2file_"$LATEST_VERSION"_"$TARGET"."$ARCHIVE" -o "$TEMP_DIR"/json2file.tar.gz
+    curl -sf https://github.com/tj-actions/json2file/releases/download/"$LATEST_VERSION"/json2file_"$LATEST_VERSION"_"$TARGET"."$ARCHIVE" -o "$TEMP_DIR"/json2file.tar.gz
     tar -xzf "$TEMP_DIR"/json2file.tar.gz -C "$TEMP_DIR"
     chmod +x "$TEMP_DIR"/json2file
   fi

d89bc31f08be9b1d9ddcb4b03b0711cfe05ad07e
Tonye Jack <jtonye@ymail.com>
2022-12-19 03:14:10 -0700

    debug curl option

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,6 +20,8 @@ if [[ -z "$INPUT_BIN_PATH" ]]; then
     TARGET=$WINDOWS_TARGET
   fi
 
+  curl --help
+
   if [[ "$ARCHIVE" == "zip" ]]; then
     curl --connect-timeout=300 -sLf https://github.com/tj-actions/json2file/releases/download/"$LATEST_VERSION"/json2file_"$LATEST_VERSION"_"$TARGET"."$ARCHIVE" -o "$TEMP_DIR"/json2file.zip
     unzip -q "$TEMP_DIR"/json2file.zip -d "$TEMP_DIR"

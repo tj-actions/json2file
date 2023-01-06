18dde0a511feba3bd1444407196037d7111a8bf9
Tonye Jack <jtonye@ymail.com>
2023-01-06 13:22:16 -0700

    Updated to split code into modules

--- src/main.rs
+++ src/main.rs
@@ -1,138 +1,15 @@
-use clap::Parser;
 use std::env;
-use std::fs::File;
-use std::io::{ErrorKind, Write};
 use std::path::PathBuf;
 
-fn write_outputs(
-    skip_missing_keys: &bool,
-    keys: &Vec<String>,
-    output: &str,
-    output_directory: &PathBuf,
-    output_extension: &str,
-    verbose: &bool,
-) {
-    if *verbose {
-        println!("Parsing outputs...");
-    }
-    let json: serde_json::Value = match serde_json::from_str(output) {
-        Ok(json) => {
-            if *verbose {
-                println!("Outputs parsed successfully.");
-            }
-            json
-        }
-        Err(e) => {
-            eprintln!("Error parsing output: {}", e);
-            std::process::exit(1);
-        }
-    };
+use clap::Parser;
 
-    // Create the output directory if it doesn't exist
-    if !output_directory.exists() {
-        if *verbose {
-            println!("Creating output directory...");
-        }
+use writer::write_outputs;
 
-        match std::fs::create_dir_all(output_directory) {
-            Ok(_) => {
-                if *verbose {
-                    println!("Output directory created successfully.");
-                }
-            }
-            Err(e) => {
-                eprintln!(
-                    "Error creating output directory '{}': {}",
-                    output_directory.display(),
-                    e
-                );
-                std::process::exit(1);
-            }
-        }
-    } else if *verbose {
-        println!("Output directory already exists.");
-    }
-
-    for key in keys {
-        if *verbose {
-            println!("Writing output for key '{}'...", key);
-        }
-        let value = match json.get(key) {
-            Some(value) => value.as_str().unwrap(),
-            None => {
-                if *skip_missing_keys {
-                    continue;
-                } else {
-                    eprintln!("Invalid key \"{}\" not found in output {}", key, output);
-                    std::process::exit(1);
-                }
-            }
-        };
-        let file_path = &output_directory.join(format!("{}.{}", key, output_extension));
-        if *verbose {
-            println!("Writing output to file '{}'...", file_path.display());
-        }
-        let mut file = match File::create(file_path) {
-            Ok(file) => file,
-            Err(err) => {
-                if err.kind() == ErrorKind::AlreadyExists {
-                    eprintln!("File '{}' already exists.", file_path.display());
-                    std::process::exit(1);
-                } else {
-                    eprintln!("Error creating file '{}': {}", file_path.display(), err);
-                    std::process::exit(1);
-                }
-            }
-        };
-
-        if let Err(err) = file.write_all(value.to_string().as_bytes()) {
-            eprintln!("Error writing to file '{}': {}", file_path.display(), err);
-            std::process::exit(1);
-        }
-
-        if *verbose {
-            println!(
-                "Wrote output to file '{}' for key '{}'.",
-                file_path.display(),
-                key
-            );
-        }
-    }
-
-    if *verbose {
-        println!("Finished writing outputs.");
-    }
-}
-
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-    /// Space delimited list of keys to extract from the JSON output converted into Vec<String>.
-    #[clap(short, long, required = true)]
-    keys: String,
-
-    /// The JSON output to use.
-    #[arg(short, long, required = true)]
-    outputs: String,
-
-    /// The directory to output the files to.
-    #[arg(short, long, required = true)]
-    directory: String,
-
-    /// Skip missing keys.
-    #[arg(short, long)]
-    skip_missing_keys: bool,
-
-    /// The extension to use for the files.
-    #[arg(short, long, default_value = "txt")]
-    extension: String,
-
-    #[arg(short, long)]
-    verbose: bool,
-}
+mod args;
+mod writer;
 
 fn main() {
-    let args = Args::parse();
+    let args = args::Args::parse();
 
     let keys: Vec<String> = args
         .keys
@@ -163,12 +40,14 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use std::fs::File;
     use std::io::Read;
+
+    use super::*;
 
     #[test]
     fn test_args() {
-        let args = Args::parse_from(&[
+        let args = args::Args::parse_from(&[
             "",
             "--keys",
             "key1 key2",
@@ -189,7 +68,7 @@ mod tests {
 
     #[test]
     fn test_main() {
-        let args = Args::parse_from(&[
+        let args = args::Args::parse_from(&[
             "",
             "--keys",
             "key1 key2",

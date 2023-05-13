db0d8ea18ba93306ce99f189783d725844b47706
Tonye Jack <jtonye@ymail.com>
2023-05-12 23:32:50 -0600

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,14 @@
-// The json2file library package provides a convenient way to write JSON data to file(s) using Rust.
-//
-// It includes a `write_outputs` function that takes input JSON data and a set of arguments specifying the output file(s) to write to, and writes the data to the specified file(s) in the specified format.
-//
-// This library is designed to be easy to use and flexible, with support for writing JSON data to multiple files in different formats, including JSON, and plain text.
+// Copyright ⓒ 2021 Tonye Jack.
+// Licensed under the MIT license
+// (see LICENSE or <http://opensource.org/licenses/MIT>).
+
+
+//! # json2file provides a convenient way to write JSON data to file(s).
+//! 
+//! It includes a [writer](crate::writer) module that takes input JSON data and a set of arguments specifying the output file(s) to write to, and writes the data to the specified file(s) in the specified format.
+//! 
+//! This library is designed to be easy to use and flexible, with support for writing JSON data to multiple files in different formats, including JSON, and plain text.
+
+#![deny(missing_docs)]
 
 pub mod writer;

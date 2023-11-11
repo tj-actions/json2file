80a4bb330543e8e74aa41c15a74d9a576e165eea
Tonye Jack <jtonye@ymail.com>
2023-11-10 21:06:57 -0700

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@
 
 //! # json2file provides a convenient way to write JSON data to file(s).
 //!
-//! It includes a [writer](crate::writer) module that takes input JSON data and a set of arguments specifying the output file(s) to write to, and writes the data to the specified file(s) in the specified format.
+//! It includes a [crate::writer] module that takes input JSON data and a set of arguments specifying the output file(s) to write to, and writes the data to the specified file(s) in the specified format.
 //!
 //! This library is designed to be easy to use and flexible, with support for writing JSON data to multiple files in different formats, including JSON, and plain text.
 
